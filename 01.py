5041bc44a88933b43130b031c4c5c8fbb876447d
Sofi <26.12.13.sofia@gmail.com>
2021-01-05 12:38:06 +0100

    input

--- 01.py
+++ 01.py
@@ -8,3 +8,21 @@ def line(board):
               board[1+i*3], '|', board[2+i*3], '|')
         print('_' * 10)
 
+
+def start_input(player_symbol):
+    act = False
+    while not act
+    player_answer = input('Where to put' + player_symbol + ' ?')
+    try:
+    player_answer = int(player_answer)
+    except:
+        print('Are you sure you entered the number?')
+        continue
+    if player_answer >= 1 and player_answer <= 9:
+        if (str(board[player_answer - 1])not in "XO"):
+            board[player_answer - 1] = player_symbol
+            act = True
+        else:
+            print("This place is already occupied.")
+    else:
+        print("Enter  number ftom1 to 9.")
